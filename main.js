c5b385c4bd231a005998a88c572b13b9f8282132
Florik <132938276+FlorikProg@users.noreply.github.com>
2024-07-13 08:08:53 +0000

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function addMessage(title, content) {
-    var messageBox = createMessageBox(title, content);
-    var messageBoxes = document.querySelector(".message-boxes");
-    messageBoxes.insertBefore(messageBox, messageBoxes.firstChild);
-  }
-  
